63c403ed59d8ab3e5f12f9e2633e0b3a5a44e65d
Christian Munte <cmunte132@gmail.com>
2017-01-19 09:26:01 -0500

    Initial Commit

---
+++ CIS3501/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CIS3501
+//
+//  Created by Christian Munte on 1/19/17.
+//  Copyright © 2017 Christian Munte. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ Prog1/main.cpp
@@ -0,0 +1,17 @@
+//  Created by Christian Munte on 1/19/17
+//  Description: The program will be taking in first an integer with the amount of stops avalible.
+//               Then the program will take stops and list all the poisslbe ways to catch the Pokemon.
+//               Then, using next_permutation the program will find the shortest possible path.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
