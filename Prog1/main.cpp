4d143ddec4dcc7c10ee496a18028c15081f40a9d
Christian Munte <cmunte132@gmail.com>
2017-01-19 10:51:55 -0500

    Added Import Pokemon can now be imported as defined by the program

--- Prog1/main.cpp
+++ Prog1/main.cpp
@@ -11,7 +11,48 @@
 
 using namespace std;
 
+class ImportPoke {
+private:
+    struct Pokemon {
+        string name;
+        int xPos;
+        int yPos;
+    };
+    vector <Pokemon> pokeStops;
+public:
+    void import(int);
+    void addPokemon(string, int, int);
+    void printStops();
+};
+
 int main() {
-    
+    ImportPoke pokemon;
+    int loop;
+    cin >> loop;
+    pokemon.import(loop);
+    pokemon.printStops();
     return 0;
+}
+
+void ImportPoke::import(int loop){
+    for (int a=0; a<loop; a++) {
+        string name;
+        int x, y;
+        cin >> x >> y >> name;
+        addPokemon(name, x, y);
+    }
+}
+
+void ImportPoke::addPokemon(string name, int x, int y) {
+    struct Pokemon temp;
+    temp.name = name;
+    temp.xPos = x;
+    temp.yPos = y;
+    pokeStops.push_back(temp);
+}
+
+void ImportPoke::printStops(){
+    for (int a=0; a<pokeStops.size(); a++) {
+        cout << pokeStops[a].name << " at " << pokeStops[a].xPos << "," << pokeStops[a].yPos << endl;
+    }
 }
